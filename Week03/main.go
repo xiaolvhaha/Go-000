e8bbd78aeae9bc21d65a6923cd5b64e1621b5a6e
xiaolv17 <lvyi17@163.com>
2020-12-09 01:03:04 +0800

    'week03'

--- Week03/main.go
+++ Week03/main.go
@@ -37,17 +37,26 @@ func main(){
 	c := make(chan os.Signal, 3)
 	signal.Notify(c, syscall.SIGHUP, os.Kill,syscall.SIGINT)
 
-	select {
-	case <-c:
-		//理论上需要注册一下这里可以打印出收到了哪种信号
-		fmt.Println("收到信号")
-		time.Sleep(10*time.Second)
-		fmt.Println("退出")
-		cancel()
+	g.Go(func() error {
+		select {
+		case <-c:
+			//理论上需要注册一下这里可以打印出收到了哪种信号
+			fmt.Println("收到信号")
+			time.Sleep(10*time.Second)
+			fmt.Println("退出")
+			cancel()
 
-	case <-ctx.Done():
-		fmt.Println("退出2")
-		server.Shutdown(ctx)
+		case <-ctx.Done():
+			fmt.Println("退出2")
+			server.Shutdown(ctx)
+		}
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Println(err)
 	}
+
+
 
 }
